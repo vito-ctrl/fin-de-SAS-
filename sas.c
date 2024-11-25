88b7daf586d9bcf36a720dfa74a1f6da0ba540ce
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2024-11-25 16:18:14 +0100

    add switch and condetions for the menu

--- sas.c
+++ sas.c
@@ -10,8 +10,12 @@ struct details {
 
 int main() {
     struct details tasks[100];
+    int tk = 0;
+    int capacity = 2;
+    int quit = 1;
     int option;
-    
+
+    while (quit == 1) {
     printf("\n<<<<<<<<<<<<<<<<menu>>>>>>>>>>>>>>>>>>>\n");
         printf("\n1 .    add a task      . \n");
         printf("2 .  view all tasks    . \n");
@@ -23,4 +27,35 @@ int main() {
         printf("Enter an number option from 1 to 6: ");
         scanf("%d", &option);
         printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+
+        if (option >= 1 && option <= 6) {
+            switch (option) {
+                case 1:
+                    tk = addTask(tasks, tk);
+                    break;
+                case 2:
+                    viewTasks(tasks, tk);
+                    break;
+                case 3:
+                    editTask(tasks, tk);
+                    break;
+                case 4:
+                    tk = deleteTask(tasks, tk);
+                    break;
+                case 5:
+                    int priority;
+                    printf("Enter priority to filter by (1 for high, 0 for low): ");
+                    scanf("%d", &priority);
+                    filterByPriority(tasks, tk, priority);
+                    break;
+                case 6:
+                    quit = 0; // Exit the loop and terminate the program
+                    break;
+                }
+        } else {
+            printf("Invalid option. Please enter a number between 1 and 6.\n");
+        }
+    }
+
+    return 0;
 }
